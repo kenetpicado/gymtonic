089ae3d34f9e2053da718ebdca84fff346e2a03a
Kenet Picado <kenetpicado1@gmail.com>
2023-07-31 15:06:49 -0600

    Add metatags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,26 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    <meta name="title" content="{{ config('app.name', 'Laravel') }} - Inicio" />
+    <meta name="description"
+        content="Tu destino para un entrenamiento de primer nivel. Clases dirigidas, entrenamiento personalizado y equipos de última generación. Únete hoy y alcanza tus metas. Tu gym, tu transformación." />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://alonsogym.com" />
+    <meta property="og:title" content="{{ config('app.name', 'Laravel') }} - Inicio" />
+    <meta property="og:description"
+        content="Tu destino para un entrenamiento de primer nivel. Clases dirigidas, entrenamiento personalizado y equipos de última generación. Únete hoy y alcanza tus metas. Tu gym, tu transformación." />
+    <meta property="og:image" content="https://i.postimg.cc/QdKtqnrz/logo-ag.png" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://alonsogym.com" />
+    <meta property="twitter:title" content="{{ config('app.name', 'Laravel') }} - Inicio" />
+    <meta property="twitter:description"
+        content="Tu destino para un entrenamiento de primer nivel. Clases dirigidas, entrenamiento personalizado y equipos de última generación. Únete hoy y alcanza tus metas. Tu gym, tu transformación." />
+    <meta property="twitter:image" content="https://i.postimg.cc/QdKtqnrz/logo-ag.png" />
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
