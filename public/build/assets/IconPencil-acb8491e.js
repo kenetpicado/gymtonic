692a947d1cbef2342a3ea58e4d563098f763fb11
Kenet Picado <kenetpicado1@gmail.com>
2024-03-22 18:35:42 -0600

    build

--- public/build/assets/IconPencil-acb8491e.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./AppLayout-6457c51b.js";var c=e("pencil","IconPencil",[["path",{d:"M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4",key:"svg-0"}],["path",{d:"M13.5 6.5l4 4",key:"svg-1"}]]);export{c as I};
